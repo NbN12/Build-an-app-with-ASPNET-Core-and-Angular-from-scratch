bc5f5696aa1473993d8946c544c511177f52823e
Babyface <babyfacedeveloper@gmail.com>
2021-07-10 18:34:00 +0700

    End of section 12

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,6 +5,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
 @NgModule({
   declarations: [],
@@ -17,7 +18,8 @@ import { FileUploadModule } from 'ng2-file-upload';
     TabsModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
+    BsDatepickerModule.forRoot()
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule, FileUploadModule],
+  exports: [BsDropdownModule, BsDatepickerModule, ToastrModule, TabsModule, NgxGalleryModule, FileUploadModule],
 })
 export class SharedModule { }
